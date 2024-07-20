bddd018d111d00cbcfa581c3c3b6880a644a13a6
JustOneDeveloper | Benni <78445321+Just1Developer@users.noreply.github.com>
2024-07-21 01:57:17 +0200

    Initial Commit

---
+++ src/main/java/net/justonedev/mc/nicknames/Nicknames.java
@@ -0,0 +1,17 @@
+package net.justonedev.mc.nicknames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Nicknames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
